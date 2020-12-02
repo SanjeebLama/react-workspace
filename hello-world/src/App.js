b5f4040f53b14be106be0ead9f37826c2dd740c2
Sanjeeb <sanjeeb.lama004@gmail.com>
2020-12-02 22:29:37 +0545

    Used Functional and Class Component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h3>Here and Now</h3>
-          <h2>Breath and Relax</h2>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet />
+      <Welcome />
     </div>
   );
 }
