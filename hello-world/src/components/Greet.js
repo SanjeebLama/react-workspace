505a6917f84a21fc2b9173a3945328deaf1b6d20
Sanjeeb <sanjeeb.lama004@gmail.com>
2020-12-02 22:30:30 +0545

    Stateless - Functional Component and Stateful - Classs Component

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,12 @@
+//Greet.js is Stateless Functional Component
+import React from "react";
+
+// function Greet() {
+//   return <h1>Breath and Relax </h1>;
+// }
+
+// export const Greet = () => <h1>Here and Now </h1>;
+// import { Greet } from "./components/Greet"; this code in APP.js
+
+const Greet = () => <h2>Here and Now </h2>;
+export default Greet;
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,11 @@
+// this is Stateful Class Component
+// we have to import two things
+// Don't forget to use render()
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return <h1>Breath and Relax</h1>;
+  }
+}
+export default Welcome;
