10a03dc1f5d52ab462ebc3e029d8863ba075884a
Sanjeeb <sanjeeb.lama004@gmail.com>
2021-01-07 19:20:05 +0545

    Initial Folder Structure completed

---
+++ sns-app/src/components/sign-in-button/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInButton = () => {
+  return (
+    <div>
+      <Button>Sign In With Google</Button>
+    </div>
+  );
+};
+
+export default SignInButton;
---
+++ sns-app/src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignInButton } from "../../components";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <SignInButton />
+    </div>
+  );
+};
+
+export default Home;
